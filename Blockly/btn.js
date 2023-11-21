0e63c1924edb60bdb493ed89ccc5ebb15759226a
NJUyd2001 <958902573@qq.com>
2023-11-21 11:20:26 +0800

    添加python生成

--- Blockly/btn.js
+++ Blockly/btn.js
@@ -1,6 +1,30 @@
 var btn = document.getElementById("button");
     btn.onclick  =function(){  
-        alert("运行！");
         const code = python.pythonGenerator.workspaceToCode(workspace);
-        eval(code);
+        document.getElementById('generatedCodeContainer').value = code;
+        const BtnSaveAs = document.getElementById('btn-save-as')
+async function fileSaveAs(description) {
+    const options = {
+    types: [{
+        description,
+        accept: {
+        'text/plain': ['.txt'],
+         },
+     }, ],
+ };
+ return await window.showSaveFilePicker(options);
+}
+BtnSaveAs.addEventListener('click', async () => {
+  const handle = await fileSaveAs("Hello File Access Api")
+})
+        // const blob = new Blob([code], {
+        //     type: "text/plain;charset=utf-8",
+        // })
+        // const objectURL = URL.createObjectURL(blob);
+        // objectURL.pathname="../Blockly/test";
+        // const aTag = document.createElement('a');
+        // aTag.href = objectURL;
+        // aTag.download = "tasks.py";
+        // aTag.click();
+        // URL.revokeObjectURL(objectURL);
     }
--- Blockly/index.js
+++ Blockly/index.js
@@ -8,10 +8,10 @@ function start() {
     {
         toolbox:document.getElementById('toolbox-categories'),
     });
-  workspace.addChangeListener(event => {
-      const code = python.pythonGenerator.workspaceToCode(workspace);
-      document.getElementById('generatedCodeContainer').value = code;
-    });
+  // workspace.addChangeListener(event => {
+  //     const code = python.pythonGenerator.workspaceToCode(workspace);
+  //     document.getElementById('generatedCodeContainer').value = code;
+  //   });
   registerOutputOption();
   registerHelpOption();
   registerDisplayOption();
@@ -69,8 +69,38 @@ function registerExcelContent()
       const fieldValue = block.getFieldValue('MY_FIELD');
       const value = '\'' + block.getFieldValue('VALUE') + '\'';
       const innerCode = generator.statementToCode(block, 'MY_STATEMENT_INPUT');
+      var code ="from robocorp import browser\nfrom robocorp.tasks import task\n\nfrom RPA.Excel.Files import Files as Excel\nfrom RPA.HTTP import HTTP";
+      code +="\n@task";
+      code +="\ndef solve_challenge():"
+      code +="\n\tsrc = Excel()\n\tsrc.open_workbook(path='./data/2023.07 劳务税.xls')\n\tsrcTable = src.read_worksheet_as_table(name='3413', header=True)"
+      code +="\n\tnoneResident = Excel()";
+      code +="\n\tnoneResident.create_workbook(path='./output/非居民.xls', fmt='xls', sheet_name='非居民')";
+      code +="\n\tnoneResidentTable = []";
+      code +="\n\tlocal = Excel()";
+      code +="\n\tlocal.create_workbook(path='./output/国内.xls', fmt='xls', sheet_name='国内')";
+      code +="\n\tlocalTable = []";
+      code +="\n\tforeigner = Excel()";
+      code +="\n\tforeigner.create_workbook(path='./output/国外.xls', fmt='xls', sheet_name='国外')";
+      code +="\n\tforeignerTable = []";
+      code +="\n\tfor row in srcTable:"
+      code +="\n\t\tif int(row['劳务收入_劳务税非居民']) != 0 or int(row['劳务税率_劳务税非居民']) != 0 or \\\
+              \n\t\t\tint(row['劳务实发_劳务税非居民']) != 0 or int(row['劳务税_劳务税非居民']) != 0 or \\\
+              \n\t\t\tint(row['劳务应扣税_劳务税非居民']) != 0: ";
+      code +="\n\t\t\tnoneResidentTable.append(row)";
+      code +="\n\t\t\tcontinue";
+      code +="\n\n\t\tid = str(row['证件号'])";
+      code +="\n\t\tif len(id) == 18 and not id.startswith('83'):";
+      code +="\n\t\t\tlocalTable.append(row)";
+      code +="\n\t\t\tcontinue";
+      code +="\n\n\t\tforeignerTable.append(row)";
+      code +="\n\n\tnoneResident.append_rows_to_worksheet(content=noneResidentTable, header=True)";
+      code +="\n\tnoneResident.save_workbook()";
+      code +="\n\tlocal.append_rows_to_worksheet(content=localTable, header=True)";
+      code +="\n\tlocal.save_workbook()";
+      code +="\n\tforeigner.append_rows_to_worksheet(content=foreignerTable, header=True)";
+      code +="\n\tforeigner.save_workbook()";
       // Return code.
-      return 'solve_challenge()';
+      return code;
     }
 }
 function registerHelpOption() {
