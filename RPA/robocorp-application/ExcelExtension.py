eaefb2c02161dfd7511148713499a0704bd54149
gbccccc <78393470+gbccccc@users.noreply.github.com>
2023-12-03 22:25:52 +0800

    read column

--- RPA/robocorp-application/ExcelExtension.py
+++ RPA/robocorp-application/ExcelExtension.py
@@ -27,19 +27,45 @@ class ExcelApplicationExtension(ExcelApplication):
     ):
         if row is None:
             row = self.active_row
-        column = column_from if column_from is not None else 1
+        column_from = column_from if column_from is not None else 1
+        column = column_from
         column_num = column_to - column + 1 if column_to is not None else None
-        
+
         contents = []
         content = self.read_from_cells(row=row, column=column)
         while (
-            column_from is None
-            and content is not None
+            column_num is None
+            and content is not None  # read until None
             or column_num is not None
-            and column <= column_num
+            and column - column_from < column_num  # read until reaching column_num
         ):
             contents.append(content)
             column += 1
+            content = self.read_from_cells(row=row, column=column)
+        return contents
+
+    def read_column(
+        self,
+        column: int = None,
+        row_from: int = None,
+        row_to: int = None,
+    ):
+        if column is None:
+            column = self.active_column
+        row_from = row_from if row_from is not None else 1
+        row = row_from
+        row_num = row_to - row + 1 if row_to is not None else None
+
+        contents = []
+        content = self.read_from_cells(row=row, column=column)
+        while (
+            row_num is None
+            and content is not None  # read until None
+            or row_num is not None
+            and row - row_from < row_num  # read until reaching column_num
+        ):
+            contents.append(content)
+            row += 1
             content = self.read_from_cells(row=row, column=column)
         return contents
 
@@ -53,7 +79,7 @@ class ExcelApplicationExtension(ExcelApplication):
     ):
         row_from = row_from if row_from is not None else 1
         column_from = column_from if column_from is not None else 1
-        
+
         if header:
             column_names = self.read_row(
                 row=row_from, column_from=column_from, column_to=column_to
@@ -62,7 +88,9 @@ class ExcelApplicationExtension(ExcelApplication):
 
         row_contents = []
         for row in range(row_from, row_to + 1):
-            row_content = self.read_row(row=row, column_from=column_from, column_to=column_to)
+            row_content = self.read_row(
+                row=row, column_from=column_from, column_to=column_to
+            )
             if header:
                 row_dict = {}
                 for i in range(0, len(column_names)):
@@ -72,4 +100,3 @@ class ExcelApplicationExtension(ExcelApplication):
                 row_contents.append(row_content)
 
         return row_contents
-        
