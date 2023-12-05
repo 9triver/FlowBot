411b62b00385db85cf576789d9e9cede39ce1887
gbccccc <78393470+gbccccc@users.noreply.github.com>
2023-12-06 00:47:43 +0800

    insert row and insert column

--- RPA/robocorp-application/ExcelExtension.py
+++ RPA/robocorp-application/ExcelExtension.py
@@ -44,6 +44,18 @@ class ExcelApplicationExtension(ExcelApplication):
             content = self.read_from_cells(row=row, column=column)
         return contents
 
+    def insert_row(self, row: int = None, row_content=None):
+        if row is None:
+            row = self.active_row
+        for column in range(1, len(row_content) + 1):
+            self.write_to_cells(row=row, column=column, value=row_content[column - 1])
+
+    def insert_column(self, column: int = None, column_content=None):
+        if column is None:
+            column = self.active_column
+        for row in range(1, len(column_content) + 1):
+            self.write_to_cells(row=row, column=column, value=column_content[row - 1])
+
     def read_column(
         self,
         column: int = None,
