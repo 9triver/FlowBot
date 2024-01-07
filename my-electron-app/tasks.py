62a81884b95c68f7826e949d0588c9b3e2e7d5a0
gbccccc <78393470+gbccccc@users.noreply.github.com>
2024-01-07 15:11:38 +0800

    Delete tasks.py

--- my-electron-app/tasks.py
+++
@@ -1,9 +0,0 @@
-from robocorp.tasks import task
-
-from ExcelExtension import ExcelApplicationExtension as ExcelApplication
-
-@task
-def solve_challenge():
-    =ExcelApplication()
-    .open_application(visible=True)
-    .open_workbook()
