74c601c4377888eb47cb21801d06f71c40efbae6
gbccccc <78393470+gbccccc@users.noreply.github.com>
2024-01-07 15:03:49 +0800

    directory structure changed

--- my-electron-app/RPA/test/ExcelExtension.py
+++ my-electron-app/RPA/test/ExcelExtension.py
@@ -48,12 +48,16 @@ class ExcelApplicationExtension(ExcelApplication):
                 contents[header] = str(content) if content is not None else None
             else:
                 contents.append(str(content) if content is not None else None)
-            
+
             column += 1
             content = self.read_from_cells(row=row, column=column)
         return contents
 
-    def insert_row(self, row: int = None, row_content = None, header_row: int = None):
+    def insert(self, row_content):
+        self.app.Rows(1).Insert()
+        self.app.Rows(1).Value = row_content
+    
+    def insert_row(self, row: int = None, row_content=None, header_row: int = None):
         if row is None:
             row = self.active_row
 
@@ -63,16 +67,28 @@ class ExcelApplicationExtension(ExcelApplication):
             for i in range(0, len(headers)):
                 header_to_column[headers[i]] = i + 1
             for header, content in row_content.items():
-                self.write_to_cells(row=row, column=header_to_column[header], value=content, number_format='@')
-        else:    
+                self.write_to_cells(
+                    row=row,
+                    column=header_to_column[header],
+                    value=content,
+                    number_format="@",
+                )
+        else:
             for column in range(1, len(row_content) + 1):
-                self.write_to_cells(row=row, column=column, value=row_content[column - 1], number_format='@')
+                self.write_to_cells(
+                    row=row,
+                    column=column,
+                    value=row_content[column - 1],
+                    number_format="@",
+                )
 
     def insert_column(self, column: int = None, column_content=None):
         if column is None:
             column = self.active_column
         for row in range(1, len(column_content) + 1):
-            self.write_to_cells(row=row, column=column, value=column_content[row - 1], number_format='@')
+            self.write_to_cells(
+                row=row, column=column, value=column_content[row - 1], number_format="@"
+            )
 
     def read_column(
         self,
@@ -130,3 +146,49 @@ class ExcelApplicationExtension(ExcelApplication):
                 row_contents.append(row_content)
 
         return row_contents
+
+
+class WorkbookDict:
+    def __init__(self):
+        self.workbook_contents = {}
+        self.headers = None
+        self.header_row = None
+
+    def contains_name(self, name: str):
+        return name in self.workbook_contents
+
+    def add_workbook(self, name: str):
+        self.workbook_contents[name] = []
+
+    def set_headers(self, headers: list[str], header_row: int):
+        self.headers = headers
+        self.header_row = header_row
+
+    def add_row(self, name: str, row_content=None):
+        if not self.contains_name(name):
+            self.add_workbook(name)
+        self.workbook_contents[name].append(row_content)
+
+    def generate_workbook_files(self, path='./'):
+        for name, row_contents in self.workbook_contents.items():
+            app = ExcelApplicationExtension()
+            app.open_application(visible=True)
+            app.add_new_workbook()
+            app.add_new_sheet(name)
+
+            if self.headers is None:
+                i = 1
+                for row_content in row_contents:
+                    app.insert_row(row=i, row_content=row_content)
+                    i += 1
+            else:
+                app.insert_row(row=self.header_row, row_content=self.headers)
+                i = 1
+                for row_content in row_contents:
+                    if i == self.header_row:
+                        i += 1
+                    app.insert_row(row=i, row_content=row_content, header_row=self.header_row)
+                    i += 1
+
+            app.save_excel_as(filename=path + name+ '.xls', file_format=56)
+            app.close_document()
